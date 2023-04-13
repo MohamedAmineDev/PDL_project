8eca78750e962656cd08fd93cdc304878dae22ca
MultiPublicFreeDevelopper <multiplatformuser22@gmail.com>
2023-04-13 22:51:45 +0100

    Second commit

---
+++ PDL_Backend/src/test/java/com/pdl/PDL_Backend/PdlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdl.PDL_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
