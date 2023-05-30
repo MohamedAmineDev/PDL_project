325228964a48f5b1386348ec0aa26abab0164c0d
Mohamed Amine BENSAFTA <mohamedamine.bensafta@sesame.com.tn>
2023-05-30 09:35:59 +0100

    Admin Command management is done !

--- front_admin/src/app/commands-that-are-waiting-for-payment/commands-that-are-waiting-for-payment.component.ts
+++ front_admin/src/app/commands-that-are-waiting-for-payment/commands-that-are-waiting-for-payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommandService } from '../services/command.service';
 import { Command } from '../models/Command';
+import { CommandDetailsService } from '../services/command-details.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-commands-that-are-waiting-for-payment',
@@ -11,6 +13,10 @@ export class CommandsThatAreWaitingForPaymentComponent implements OnInit {
   commands: Array<Command> = [];
   showModal: boolean = false;
   wantedCommand: Command = {} as Command;
+  startUpdating(_t22: number) {
+    this.commandDetailsService.storeTotalPrice(this.commands[_t22]);
+    this.router.navigate(["/command_details/"+this.commands[_t22].id]);
+  }
   prepareBeforeDelete(_t20: number) {
     this.wantedCommand= this.commands[_t20];
     this.showModal = true;
@@ -30,7 +36,7 @@ export class CommandsThatAreWaitingForPaymentComponent implements OnInit {
       }
     );
   }
-  constructor(private commandService: CommandService) {
+  constructor(private commandService: CommandService,private commandDetailsService:CommandDetailsService,private router: Router) {
     commandService.getAllCommandsThatAreWaitingForPayment()
       .subscribe(
         (data) => {
